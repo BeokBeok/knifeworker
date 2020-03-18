d53ab7b8de5d47559c9769ffb79cbcfc6bfaca85
BeokBeok <kekemusa37@nate.com>
2020-03-18 23:39:11 +0900

    [개선] 총 근무시간을 입력하고 키패드의 완료 버튼을 누를 경우 계산하도록 개선

--- app/src/main/java/com/beok/knifeworker/ext/TextViewExt.kt
+++ app/src/main/java/com/beok/knifeworker/ext/TextViewExt.kt
@@ -1,9 +1,12 @@
 package com.beok.knifeworker.ext
 
+import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.datetime.timePicker
+import com.beok.knifeworker.MainViewModel
+import com.google.android.material.textfield.TextInputEditText
 import java.util.*
 
 @BindingAdapter("showTimePicker")
@@ -11,6 +14,18 @@ fun TextView.showTimePicker(startWorkingHour: (Calendar) -> Unit) {
     setOnClickListener {
         MaterialDialog(it.context).show {
             timePicker { _, datetime -> startWorkingHour.invoke(datetime) }
+        }
+    }
+}
+
+@BindingAdapter("showWorkOffTime")
+fun TextInputEditText.showWorkOffTime(viewModel: MainViewModel) {
+    setOnEditorActionListener { textView, actionId, _ ->
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            viewModel.showWorkOffTime(textView.text.toString())
+            false
+        } else {
+            true
         }
     }
 }
